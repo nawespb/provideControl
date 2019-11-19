cd2943e36840651204a923cc9916a3b85694f1f3
Valentin <nawe.spb@gmail.com>
2019-11-19 23:05:49 +0300

    Added admin role

--- src/main/java/com/nawe/provideControl/controller/UserController.java
+++ src/main/java/com/nawe/provideControl/controller/UserController.java
@@ -4,6 +4,7 @@ import com.nawe.provideControl.domain.Role;
 import com.nawe.provideControl.domain.User;
 import com.nawe.provideControl.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
 
     @Autowired
